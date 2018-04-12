5dd72caef73e8cd9ecb6c1e841d727dce06c8bea
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-12 12:57:38 -0500

    updated

--- javascripts/loginScreen.js
+++ javascripts/loginScreen.js
@@ -10,18 +10,9 @@ function show(){
     Copy
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input id="access-code" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <input id="access-code" type="number" min="0" inputmode="numeric" pattern="[0-9]*" title="Non-negative integral number" placeholder="ACCESS CODE">
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
+     
       <button id="submit-btn" type="submit" class="btn btn-primary">Submit</button>
     </form>
     `);
