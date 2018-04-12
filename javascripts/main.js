b463fa092892adef8370676b12a06e55d627ef0c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-12 12:05:59 -0500

    added view JS files and connected to main

---
+++ javascripts/main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Main Connected");
+
+require("./loginScreen");
+require("./surveyScreen");
---
+++ javascripts/surveyScreen.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Survey Screen Connected");
+let $ = require('jquery');
