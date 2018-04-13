21277bf30649091b9f18cdb6b647f4adfa82b464
Dan Watson <danwatson00@yahoo.com>
2018-04-13 13:47:29 -0500

    styled feeling friday

--- javascripts/tapScreen.js
+++ javascripts/tapScreen.js
@@ -9,8 +9,10 @@ function show(answerObj) {
     <form>
       <img id="logo" src="/images/logo.png" alt="company logo" />
       <div class="form-group">
+      <h2 id="feeling-sentance">Feeling Friday... we listen</h2>
       <button id="submit-btn" type="submit" class="btn-lg btn btn-primary ">Tap To Begin</button>
     </form>
+    <p id="disclaimer">Children’s Privacy Statement: Data and information collected through this survey is de-identified and will be used to enhance Boys & Girls Club programs.  We will not intentionally collect or redistribute any personal information from children under the age of 18. If you think that we have collected personal information from a child under the age of 18, please contact your Club administrator.</p>
     `);
         $("#submit-btn").on('click', (e) => {
             e.preventDefault();
