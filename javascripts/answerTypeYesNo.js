8bc7a42580eb60fb0e20ca9261bdc3401d2361a3
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-12 20:04:55 -0500

    stash

--- javascripts/answerTypeYesNo.js
+++ javascripts/answerTypeYesNo.js
@@ -3,12 +3,31 @@ let $ = require('jquery');
 
 let text = ["Yes", "No"];
 let images = ["./images/like.png", "./images/dislike.png"];
+let db = require('./databaseCalls');
 
-function show(){
+function show(answers){
     $('.container').html("");
     $(".container").append(`<div id="button-set"></div>`);
 
-    
+    text.forEach((item, index)=>{
+        $("#button-set").append(`
+        <div class="row">
+        <div class="card col-sm-4" id="${item}">
+        <img class="card-img-top" src="${images[index]}" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">${item}</p>
+        </div>
+      </div>
+        </div>
+        `);
+    });
+
+    $("#button-set .card").on('click', (e)=>{
+        answers.answer = e.currentTarget.id;
+        console.log(answers);
+        db.putData(answers);
+    });
+
 
 }
 
--- javascripts/surveyScreen.js
+++ javascripts/surveyScreen.js
@@ -1,32 +1,33 @@
 "use strict";
 let $ = require('jquery'),
 fakeData = require('./fakeData'),
+    aObj = require('./answerObj'),
     db = require('./databaseCalls'),
-    yesNo = require('./answerTypeYesNo');
-    
+    yesNo = require('./answerTypeYesNo'),
+    happy = require('./answerTypeFeeling'),
+    rate = require('./answerTypeRating');
+
 function show(answerObj){
     db.getQuestionData().then((data)=>{
     $(".container").html("");
-    
+
         switch (data.AnswerType) {
             case 1:
                 //DO SOMETHING
                 console.log("1");
+                yesNo.show(answerObj);
+                // color.show(answerObj);
+                // rate.show(answerObj);
                 break;
             case 2:
                 //DO SOMETHING
                 console.log("2");
-                yesNo.show();
-                answerObj.answer = "yes";
-                db.putData(answerObj);
+                happy.show(answerObj);
                 break;
             case 3:
                 //DO SOMETHING
                 console.log("3");
-                break;
-            case 4:
-                //DO SOMETHING
-                console.log("4");
+                rate.show(answerObj);
                 break;
         }
     });
