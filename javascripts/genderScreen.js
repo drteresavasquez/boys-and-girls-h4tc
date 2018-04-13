13a9247b83ee1da10e8be0f1b20a5660d62d375c
Dan Watson <danwatson00@yahoo.com>
2018-04-13 12:26:36 -0500

    style gender

--- javascripts/genderScreen.js
+++ javascripts/genderScreen.js
@@ -10,18 +10,18 @@ function show(answers){
     var genderDiv = document.createElement("div");
     $(".container").append(genderDiv);
     genderDiv.classList.add("gender-div");
-    $(".gender-div").append(`<div class="title">${pageTitle}</div>`);
-    $(".gender-div").append(`<div id="button-set"></div>`);
+    $(".gender-div").append(`<div class="title gender-title">${pageTitle}</div>`);
+    $(".gender-div").append(`<div id="gender-btn-set"></div>`);
     let last = gender.pop();
     gender.forEach((item)=>{
-        $("#button-set").append(`
+        $("#gender-btn-set").append(`
             <button type="button" value="${item}" id=${item} class="gender-btn btn btn-lg">${item}</button><br>
         `);
     });
 
-    $("#button-set").append(`<button type="button" id=${last} value="${last}" class="no-gender-btn last-btn-item">${last}</button>`);
+    $("#gender-btn-set").append(`<button type="button" id=${last} value="${last}" class="no-gender-btn last-btn-item">${last}</button>`);
 
-    $("#button-set button").on('click', (e)=>{
+    $("#gender-btn-set button").on('click', (e)=>{
         answers.gender = e.currentTarget.value;
         survey.show(answers);
     });
