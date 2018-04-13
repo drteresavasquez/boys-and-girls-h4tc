8e54680e878087fb494dceca361502e155f01313
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-13 12:38:59 -0500

    updated gender and answer styles

--- javascripts/genderScreen.js
+++ javascripts/genderScreen.js
@@ -3,23 +3,35 @@ let $ = require('jquery'),
     survey = require('./surveyScreen');
 
 let gender = ["Male", "Female", "No Answer"],
+    images = ["./images/boy.png", "./images/girl.png"],
     pageTitle = "Gender";
 
 function show(answers){
     $(".container").html("");
-    var genderDiv = document.createElement("div");
-    $(".container").append(genderDiv);
-    genderDiv.classList.add("gender-div");
-    $(".gender-div").append(`<div class="title">${pageTitle}</div>`);
-    $(".gender-div").append(`<div id="button-set"></div>`);
+    $(".container").append(`<div id="button-set">
+    <div class="title answers">${pageTitle}</div>
+        <div class="row">
+        </div>
+    </div>`);
+
     let last = gender.pop();
-    gender.forEach((item)=>{
-        $("#button-set").append(`
-            <button type="button" value="${item}" id=${item} class="btn btn-lg">${item}</button><br>
+    gender.forEach((item, index)=>{
+        $(".row").append(`
+        <div class="card col-sm-4 thumb-cards" id="${item}">
+            <img class="card-img-top gender" src="${images[index]}" alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">${item}</p>
+            </div>
+        </div>
         `);
     });
 
     $("#button-set").append(`<button type="button" id=${last} value="${last}" class="last-btn-item">${last}</button>`);
+
+    $("#button-set .card").on('click', (e)=>{
+        answers.gender = e.currentTarget.id;
+        survey.show(answers);
+    });
 
     $("#button-set button").on('click', (e)=>{
         answers.gender = e.currentTarget.value;
